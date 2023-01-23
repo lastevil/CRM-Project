a9e98c0e3076ee52fd8ae8e51386e8b94eebfb17
lasteik <lastevilday@gmail.com>
2023-01-23 15:16:20 +0500

    Add local lib module, first time install him in maven, the add dependency to maven on your service

---
+++ crm-local-lib/src/main/java/org/unicrm/lib/LocalLibApplication.java
@@ -0,0 +1,7 @@
+package org.unicrm.lib;
+
+public class LocalLibApplication {
+    public static void main(String[] args) {
+        System.out.println("Local lib compiled");
+    }
+}
---
+++ crm-local-lib/src/main/java/org/unicrm/lib/dto/UserSimpleDto.java
@@ -0,0 +1,11 @@
+package org.unicrm.lib.dto;
+
+import lombok.Data;
+
+@Data
+public class UserSimpleDto {
+    Long id;
+    String firstName;
+    String secondName;
+    String lastName;
+}
