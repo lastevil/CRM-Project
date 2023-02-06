4d512de0426b310f7062f686d749c39dbed9606e
bafkit <konyrevpv@mail.ru>
2023-02-07 01:22:37 +0300

    changes UserDto

--- crm-local-lib/src/main/java/org/unicrm/lib/dto/UserDto.java
+++ crm-local-lib/src/main/java/org/unicrm/lib/dto/UserDto.java
@@ -1,23 +1,17 @@
 package org.unicrm.lib.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
-
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class UserDto {
-
-    private UUID uuid;
-    private String username;
+    private UUID id;
     private String firstName;
     private String lastName;
     private Long departmentId;
-    private String departmentTitle;
+    private String department;
 
 }
