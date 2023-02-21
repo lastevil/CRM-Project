aa8c47d02347bf403d1a55823cd3de1b6f08ada4
lastik <lastevilday@gmail.com>
2023-02-21 23:25:23 +0500

    Prove serviceLogic

--- analytic-service/src/main/java/org/unicrm/analytic/repositorys/TicketRepository.java
+++ analytic-service/src/main/java/org/unicrm/analytic/repositorys/TicketRepository.java
@@ -1,14 +1,12 @@
 package org.unicrm.analytic.repositorys;
 
-import org.apache.kafka.common.protocol.types.Field;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.unicrm.analytic.api.Status;
 import org.unicrm.analytic.entities.Ticket;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -28,19 +26,21 @@ public interface TicketRepository extends JpaRepository<Ticket, UUID> {
     @Query(value = "select t from Ticket t where t.department.id=:departmentId and t.createdAt between :beginTime and :endTime")
     List<Ticket> findAllByDepartmentAssigneeId(Long departmentId, LocalDateTime beginTime, LocalDateTime endTime);
 
-    @Query(value = "select status, count(*) from tickets where department_id=:departmentId and due_date between :beginTime and :endTime group by status ", nativeQuery = true)
-    Map<Status, Integer> countByStatusDueDateBetweenDepAndDepartment(Long departmentId, LocalDateTime beginTime, LocalDateTime endTime);
-
-    @Query(value = "select overdue_status, count(*) from tickets where department_id=:departmentId and due_date between :beginTime and :endTime group by overdue_status", nativeQuery = true)
-    Map<Status, Integer> countByOverdueDueDateBetweenAndDepartment(Long departmentId, LocalDateTime beginTime, LocalDateTime endTime);
-
-    @Query(value = "select status, count(*) from tickets where assignee_id=:userId and due_date between :beginTime and :endTime group by status", nativeQuery = true)
-    Map<Status, Integer> countByStatusDueDateBetweenAndAssignee(UUID userId, LocalDateTime beginTime, LocalDateTime endTime);
-
-    @Query(value = "select overdue_status, count(*) from tickets where assignee_id=:userId and due_date between :beginTime and :endTime group by overdue_status", nativeQuery = true)
-    Map<Status, Integer> countByOverdueDueDateBetweenAndAssignee(UUID userId, LocalDateTime beginTime, LocalDateTime endTime);
-    @Query(value = "SELECT count(*) from tickets where assignee_id=:userId and due_date between :beginTime and :endTime",nativeQuery = true)
+    @Query(value = "SELECT count(*) from tickets where assignee_id=:userId and due_date between :beginTime and :endTime", nativeQuery = true)
     Integer countByAssigneeAndDueDateBetween(UUID userId, LocalDateTime beginTime, LocalDateTime endTime);
-    @Query(value = "SELECT count(*) from tickets where department_id=:departmentId and due_date between :beginTime and :endTime",nativeQuery = true)
+
+    @Query(value = "SELECT count(*) from tickets where department_id=:departmentId and due_date between :beginTime and :endTime", nativeQuery = true)
     Integer countByDepartmentAndDueDateBetween(Long departmentId, LocalDateTime beginTime, LocalDateTime endTime);
+
+    @Query(value = "select count(*) from tickets where status=:status and assignee_id=:userId and due_date between :beginTime and :endTime group by status " +
+            "union " +
+            "select count(*) from tickets where overdue_status=:status and assignee_id=:userId and due_date between :beginTime and :endTime group by overdue_status"
+            , nativeQuery = true)
+    Integer countTicketAssigneeByStatusDueDateBetweenTime(UUID userId, String status, LocalDateTime beginTime, LocalDateTime endTime);
+
+    @Query(value = "select count(*) from tickets where status=:status and department_id=:departmentId and due_date between :beginTime and :endTime group by status " +
+            "union " +
+            "select count(*) from tickets where overdue_status=:status and department_id=:departmentId and due_date between :beginTime and :endTime group by overdue_status"
+            , nativeQuery = true)
+    Integer countTicketDepartmentByStatusDueDateBetweenTime(Long departmentId, String status, LocalDateTime beginTime, LocalDateTime endTime);
 }
