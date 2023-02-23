9cd4a5976404d1411a79e3b97923aa272e8f4b9f
lastik <lastevilday@gmail.com>
2023-02-23 23:46:07 +0500

    Исправление кафки

--- chat-service/src/main/java/org/unicrm/chat/config/KafkaConfig.java
+++ chat-service/src/main/java/org/unicrm/chat/config/KafkaConfig.java
@@ -10,6 +10,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.unicrm.chat.dto.kafka.KafkaUserDto;
 
@@ -30,7 +31,10 @@ public class KafkaConfig {
         Map<String, Object> consumer = new HashMap<>();
         consumer.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         consumer.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class);
-        consumer.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumer.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        consumer.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        consumer.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        consumer.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "org.unicrm.chat.dto.kafka.KafkaUserDto");
         consumer.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         consumer.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         return consumer;
