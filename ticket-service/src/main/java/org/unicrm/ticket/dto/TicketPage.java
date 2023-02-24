48ddef539b9e9f686996a141df504b384dbef63b
basilomp <k0m4w1@gmail.com>
2023-02-24 15:15:32 +0300

    Fixing paging error.

--- ticket-service/src/main/java/org/unicrm/ticket/dto/TicketPage.java
+++ ticket-service/src/main/java/org/unicrm/ticket/dto/TicketPage.java
@@ -2,16 +2,23 @@ package org.unicrm.ticket.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Validated
 public class TicketPage {
 
+    @Min(value = 1)
     @Schema(description = "Индекс страницы со списком заявок", example = "1")
     private int page;
 
+    @Min(value = 5)
     @Schema(description = "Количество заявок для отображения на странице", example = "15")
-    private int countElements;
+    private int size;
+
 }
