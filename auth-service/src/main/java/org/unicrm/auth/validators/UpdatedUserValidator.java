22d7d06fba3785bc5d836611dc24d3bce598b74b
bafkit <konyrevpv@mail.ru>
2023-02-27 22:54:09 +0300

    Auth: Adjustments

--- auth-service/src/main/java/org/unicrm/auth/validators/UpdatedUserValidator.java
+++ auth-service/src/main/java/org/unicrm/auth/validators/UpdatedUserValidator.java
@@ -15,10 +15,10 @@ public class UpdatedUserValidator {
         List<String> errors = new ArrayList<>();
 
         if (updatedUserDto.getUsername() == null) errors.add("Username must not be null");
-        if (updatedUserDto.getFirstName() != null) if (updatedUserDto.getFirstName().isBlank()) errors.add("First name must not be blank");
-        if (updatedUserDto.getLastName() != null) if (updatedUserDto.getLastName().isBlank()) errors.add("Last name must not be blank");
-        if (updatedUserDto.getPassword() != null) if (updatedUserDto.getPassword().isBlank()) errors.add("Password must not be blank");
-        if (updatedUserDto.getEmail() != null) if (updatedUserDto.getEmail().isBlank()) errors.add("Email must not be blank");
+        if (updatedUserDto.getFirstName() != null && updatedUserDto.getFirstName().isBlank()) errors.add("First name must not be blank");
+        if (updatedUserDto.getLastName() != null && updatedUserDto.getLastName().isBlank()) errors.add("Last name must not be blank");
+        if (updatedUserDto.getPassword() != null && updatedUserDto.getPassword().isBlank()) errors.add("Password must not be blank");
+        if (updatedUserDto.getEmail() != null && updatedUserDto.getEmail().isBlank()) errors.add("Email must not be blank");
         if (!errors.isEmpty()) throw new ValidationException(errors);
     }
 }
